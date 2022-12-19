4e36fed19f468f767ff68e8f4639a21cccc52c4f
Istifar <istifarriyanti27@students.unnes.ac.id>
2022-12-19 17:35:12 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://deployment-finalproject.web.app" // TODO: replace this
+const studentName = "Istifar Riyanti" // TODO: replace this
+const studentId = "FE2768094" // TODO: replace this
+
+export { url, studentName, studentId }
